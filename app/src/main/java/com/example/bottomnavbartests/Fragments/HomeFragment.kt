accdaa849d61e9dcff4b754feff0ab928d2068eb
SaginiChan <boazsagini62@gmail.com>
2020-08-28 21:44:45 +0300

    In this branch, updated glide of loading images into project. However, it may be buggy on the gradle part. I am yet to fix

--- app/src/main/java/com/example/bottomnavbartests/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/bottomnavbartests/Fragments/HomeFragment.kt
@@ -1,16 +1,21 @@
 package com.example.bottomnavbartests.Fragments
 
+import android.app.Activity
 import android.app.Person
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.bottomnavbartests.Models.User
 import com.example.bottomnavbartests.R
 import com.example.bottomnavbartests.Util.FirestoreUtil
+import com.example.bottomnavbartests.Util.StorageUtil
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -22,7 +27,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
 import java.lang.Exception
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.request.RequestOptions
+
 
 class HomeFragment : Fragment() {
 
@@ -44,37 +53,82 @@ class HomeFragment : Fragment() {
                     action = Intent.ACTION_GET_CONTENT
                     putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png"))
                 }
-                startActivityForResult(Intent.createChooser(intent, "Select Image"), RC_SELECT_IMAGE)
+                startActivityForResult(
+                    Intent.createChooser(intent, "Select Image"),
+                    RC_SELECT_IMAGE
+                )
             }
 
 
             submitbutton.setOnClickListener {
-                FirestoreUtil.updateCurrentUser(
-                    edittextPersonname.text.toString(),
-                    editTextBio.text.toString(),
-                    editTextTextEmailAddress.text.toString(),
-                    edittextage.text.toString()
-
+                if (::selectedImageBytes.isInitialized)
+                    StorageUtil.uploadProfilePhoto(selectedImageBytes) { imagePath ->
+                        FirestoreUtil.updateCurrentUser(
+                            edittextPersonname.text.toString(),
+                            editTextBio.text.toString(),
+                            editTextTextEmailAddress.text.toString(),
+                            edittextage.text.toString(), imagePath
+                        )
+                    }
+                else
+                    FirestoreUtil.updateCurrentUser(
+                        edittextPersonname.text.toString(),
+                        editTextBio.text.toString(),
+                        editTextTextEmailAddress.text.toString(),
+                        edittextage.text.toString(), null
                     )
-                Toast.makeText(this@HomeFragment.requireActivity(), "saving", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@HomeFragment.requireActivity(), "saving", Toast.LENGTH_SHORT)
+                    .show()
             }
+        }
+
 
             return view
         }
 
-    }
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            if (requestCode == RC_SELECT_IMAGE && resultCode == Activity.RESULT_OK &&
+                data != null && data.data != null
+            ) {
+
+                val selectedImagePath = data.data
+                val selectedImageBmp = MediaStore.Images.Media
+                    .getBitmap(activity?.contentResolver, selectedImagePath)
+
+                val outputStream = ByteArrayOutputStream()
+                selectedImageBmp.compress(Bitmap.CompressFormat.JPEG, 90, outputStream)
+                selectedImageBytes = outputStream.toByteArray()
+
+                Glide.with(this)
+                    .load(selectedImageBytes)
+                    .into(imageView_profile_picture)
+
+                pictureJustChanged = true
+            }
+
+        }
 
 
-    override fun onStart() {
-        super.onStart()
-        FirestoreUtil.getCurrentUser { user ->
-            if (this@HomeFragment.isVisible){
-                edittextPersonname.setText(user.name)
-                editTextBio.setText(user.bio)
-                editTextTextEmailAddress.setText(user.email)
-                edittextage.setText(user.age)
 
+            override fun onStart() {
+                super.onStart()
+                FirestoreUtil.getCurrentUser { user ->
+                    if (this@HomeFragment.isVisible) {
+                        edittextPersonname.setText(user.name)
+                        editTextBio.setText(user.bio)
+                        editTextTextEmailAddress.setText(user.email)
+                        edittextage.setText(user.age)
+                        if (!pictureJustChanged && user.profilePicturePath != null)
+
+
+                            Glide.with(this)
+                                .load(StorageUtil.pathToReference(user.profilePicturePath))
+                                .apply(RequestOptions()
+                                    .placeholder(R.drawable.ic_circle))
+                                .into(imageView_profile_picture)
+
+                    }
+                }
             }
         }
-    }
-}
+
